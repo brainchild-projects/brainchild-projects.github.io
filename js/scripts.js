e11d7aab254819a9d9e8f7292dc25f23c40d3621
asartalo <asartalo@gmail.com>
2025-02-27 04:58:02 +0000

    Rebuilding site Thu Feb 27 04:58:02 UTC 2025

--- js/scripts.js
+++
@@ -1,6 +0,0 @@
-import OverlayElement from './grid_overlay.js';
-import tagCloud from './tag_cloud.js';
-// toggle overlay
-const overlay = new OverlayElement('.container');
-overlay.listen();
-tagCloud();
